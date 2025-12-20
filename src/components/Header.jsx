b670126ee5babc30c6cc28eba6e72530f09e6aee
kazari4 <skidyuk86115@gmail.com>
2025-12-20 17:20:04 +0900

    headerのテキストをボタンからただのテキストに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ export default function Header() {
     return (
         <header className="navbar bg-base-100 shadow-md">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">ラーメンズ visualizer</a>
+                <span className="text-xl font-bold">
+                    ラーメンズ visualizer
+                </span>
             </div>
         </header>
     );
