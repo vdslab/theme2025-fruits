87f82421053559f1ffcb450630d57955e3508ade
kazari4 <skidyuk86115@gmail.com>
2025-12-20 17:08:14 +0900

    Headerを追加

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="navbar bg-base-100 shadow-md">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">ラーメンズ visualizer</a>
+            </div>
+        </header>
+    );
+}
