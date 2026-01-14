e70c340d655d0f93580ed68e6f541aeaf07dd43b
ju.dz <hasejun218@gmail.com>
2026-01-14 17:45:42 +0900

    ヘッダーを消した

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header className="navbar bg-base-100 shadow-md">
-            <div className="flex-1">
-                <span className="text-xl font-bold">ラーメンズ visualizer</span>
-            </div>
-        </header>
-    );
-}
