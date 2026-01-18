22ebdeceffd79733f4c22f5d1f63b8dc706b4f10
kazari4 <skidyuk86115@gmail.com>
2026-01-18 15:46:48 +0900

    色を調整

--- src/lib/getContNodeClass.js
+++ src/lib/getContNodeClass.js
@@ -1,23 +1,20 @@
 export function getContNodeClass(performanceId, emphasis) {
     const CONT_ID_TO_COLOR = {
-        5: "fill-blue-500",
-        6: "fill-blue-600",
-        7: "fill-blue-700",
+        5: "fill-[#1F77B4]", // blue
+        6: "fill-[#FF7F0E]", // orange
+        7: "fill-[#2CA02C]", // green
+        8: "fill-[#17BECF]", // cyan
+        9: "fill-[#D62728]", // red
 
-        8: "fill-green-600",
-        9: "fill-green-700",
+        10: "fill-[#9467BD]", // purple
+        11: "fill-[#8C564B]", // brown
+        12: "fill-[#E377C2]", // pink-magenta
+        13: "fill-[#7F7F7F]", // gray
 
-        10: "fill-purple-600",
-        11: "fill-purple-700",
-
-        12: "fill-orange-600",
-        13: "fill-orange-700",
-
-        14: "fill-red-600",
-        15: "fill-red-700",
-
-        16: "fill-teal-600",
-        17: "fill-indigo-700",
+        14: "fill-[#BCBD22]", // olive
+        15: "fill-[#AEC7E8]", // light blue
+        16: "fill-[#FF9896]", // light red
+        17: "fill-[#C49C94]", // light brown
     };
     const base = CONT_ID_TO_COLOR[performanceId];
     if (!base) return "fill-gray-400";
